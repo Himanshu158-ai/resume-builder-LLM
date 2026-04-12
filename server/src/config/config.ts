514b4420d1bd405f64e060680aa8ca86dc0f108f
Himanshu Singh <hs5317610@gmail.com>
2026-04-12 15:48:40 +0530

    feat: initialize Node.js server with Express, TypeScript, and LangChain dependencies

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    port: process.env.PORT || 3000,
     geminiApiKey: process.env.GEMINI_API_KEY,
     mistralApiKey: process.env.MISTRAL_API_KEY,
     cohereApiKey: process.env.COHERE_API_KEY,
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,39 @@
 import express from "express";
-import { config } from "./config/config";
-import ResumeRoute from "./routes/ResumeRoute";
 import cors from "cors";
-import { Request, Response } from "express";
+import dotenv from "dotenv";
+import ResumeRoute from "./routes/ResumeRoute";
 
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 const allowedOrigins =
-    process.env.NODE_ENV === "production"
-        ? [process.env.CLIENT_URL]
-        : ["http://localhost:5173", "http://127.0.0.1:5173"]
+  process.env.NODE_ENV === "production"
+    ? [process.env.CLIENT_URL as string]
+    : ["http://localhost:5173", "http://127.0.0.1:5173"];
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true)
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
 
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error("Not allowed by CORS"))
-        }
-    },
-    credentials: true
-}))
-
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use("/api/resume", ResumeRoute);
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Server is running");
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
 });
 
-app.listen(config.port, () => {
-    console.log(`Server is running on port ${config.port}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
